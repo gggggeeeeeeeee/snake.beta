ef876f48f738c698097104918058d7050e4c18ef
gggggeeeeeeeee <47082842+gggggeeeeeeeee@users.noreply.github.com>
2019-02-24 17:53:18 +0300

    Add files via upload
    змея теперь не бета

--- shake.js
+++ shake.js
@@ -1,6 +1,7 @@
 let petla;
 let w;
 
+
 function setup() {
   createCanvas(400,400);
 petla = new Petla();
@@ -17,6 +18,7 @@ background(200);
 petla.show();
 petla.crawl();
 petla.telep();
+petla.Lose();
 milo.show();
 if(petla.isFoodPicked(milo)){
 milo.pickLocation();
@@ -29,33 +31,41 @@ this.x = 200;
 this.y = 200;
 this.speedx=0;
 this.speedy=0;
-this.food;
+this.food = 0;
 this.tail=[];
 
 this.show = function() {
   rect(this.x,this.y,w,w);
-for(let i = 0; i < this.tail.length;i++){
-  rect
+
+for (let i = 0; i < this.tail.length; i++){
+  rect(this.tail[i].x,this.tail[i].y,w,w);
 }
 };
+
 this.crawl = function(){
+for (let i = 0; i < this.tail.length - 1; i++){
+this.tail[i] = this.tail [i + 1];
+}
+
+this.tail[this.food - 1] = {
+  x: this.x,
+  y:this.y,
+};
+
 this.x+=this.speedx * w;
 this.y+=this.speedy * w;
-for (let i = 0; i < this.food - 1; i++){
-tail[i] = tail[i+1];
-}
-tail[this.food - 1].x = this.x;
-tail[this.food - 1].y = this.y;
+
 };
 
 this.direction = function(dirX,dirY){
 this.speedx= dirX;
 this.speedy= dirY;
 };
+
 this.telep = function(){
-if(this.x < 0){this.x=width;}
+if(this.x < 0){this.x=width-w;}
 if(this.x > 400){this.x=0;}
-if(this.y < 0){this.y=width;}
+if(this.y < 0){this.y=width-w;}
 if(this.y > 400){this.y=0;}
 };
 
@@ -63,11 +73,27 @@ if(this.y > 400){this.y=0;}
 this.isFoodPicked = function(other) {
 if(this.x === other.x && this.y === other.y){
 this.food+=1;
-  return true;
+return true;
 }
 return false;
 };
+this.Lose = function () {
+  for (let i = 0; i < this.tail.length - 1; i++){
+if(this.x == this.tail[i].x)
+{
+  if(this.y == this.tail[i].y){
+    this.x = 200;
+    this.y = 200;
+    this.speedx=0;
+    this.speedy=0;
+    this.food = 0;
+
 }
+}
+}
+
+
+}}
 function keyPressed() {
 if(keyCode === UP_ARROW) {
 petla.direction(0,-1);
@@ -99,5 +125,4 @@ function miloClass() {
     fill("#FF0D9f");
     rect(this.x,this.y,w,w);
   };
-
 }
